4f3a12c4bfacd4e2e78b493c9b6576537466b288
James Young <jyyoung@jyymail.com>
2026-09-06 18:34:28 -0400

    feat: add db registry for ORM models to use in conftest.py and env.py

---
+++ src/jyyfinhub_spendtracker/db/registry.py
@@ -0,0 +1,5 @@
+import jyyfinhub_spendtracker.payment_methods.models  # noqa: F401
+from jyyfinhub_spendtracker.db.base import Base
+from jyyfinhub_spendtracker.transactions import models  # noqa: F401
+
+__all__ = ["Base"]
