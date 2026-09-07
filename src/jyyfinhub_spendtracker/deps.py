164ee538a469ea9127ef708b65a168dce47e3d56
James Young <jyyoung@jyymail.com>
2026-09-07 16:23:15 -0400

    fix: SessionDep should be AsyncSession

--- src/jyyfinhub_spendtracker/deps.py
+++ src/jyyfinhub_spendtracker/deps.py
@@ -1,9 +1,9 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from jyyfinhub_spendtracker.db.session import get_session
 
-# saves writing `session: Session = Depends(get_session)` in every route
-SessionDep = Annotated[Session, Depends(get_session)]
+# saves writing `session: AsyncSession = Depends(get_session)` in every route
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
