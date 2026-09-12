3841a15ba5c043084c9b31c8454adbcd8582872d
James Young <jyyoung@jyymail.com>
2026-09-12 19:52:44 -0400

    feat: add transaction api endpoints to fastapi router

--- src/jyyfinhub_spendtracker/api.py
+++ src/jyyfinhub_spendtracker/api.py
@@ -5,7 +5,9 @@ from fastapi import APIRouter
 from jyyfinhub_spendtracker.payment_methods.router import (
     router as payment_method_router,
 )
+from jyyfinhub_spendtracker.transactions.router import router as transaction_router
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(payment_method_router)
+api_router.include_router(transaction_router)
